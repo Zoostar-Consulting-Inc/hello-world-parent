52d9df49756869ce079da5a8c9740f924f4b0ccc
zoostar <devops@zoostar.net>
2021-04-13 09:41:25 -0400

    Sonar fix

--- hello-world-web/src/test/java/net/zoostar/hw/web/controller/api/UserRestControllerTest.java
+++ hello-world-web/src/test/java/net/zoostar/hw/web/controller/api/UserRestControllerTest.java
@@ -92,7 +92,7 @@ class UserRestControllerTest {
 		ResponseEntity<User> response = controller.create(request);
 		
 		//THEN
-		assertTrue("UserRequest(email=user1@email.com, firstName=User1, lastName=Last)".equals(request.toString()));
+		assertEquals("UserRequest(email=user1@email.com, firstName=User1, lastName=Last)", request.toString());
 		assertNotNull(response);
 		assertEquals(HttpStatus.CREATED, response.getStatusCode());
 		User user = response.getBody();
