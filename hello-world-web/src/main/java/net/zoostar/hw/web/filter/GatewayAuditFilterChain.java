a4683f6cf3244583264c5eeb1be3edb50419c770
zoostar <devops@zoostar.net>
2021-04-27 16:54:33 -0400

    HW-6: Add authentication

--- hello-world-web/src/main/java/net/zoostar/hw/web/filter/GatewayAuditFilterChain.java
+++ hello-world-web/src/main/java/net/zoostar/hw/web/filter/GatewayAuditFilterChain.java
@@ -1,5 +1,8 @@
 package net.zoostar.hw.web.filter;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Component;
@@ -13,22 +16,33 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class GatewayAuditFilterChain extends AbstractRequestLoggingFilter {
 
+	private static final List<String> EXCLUDED_ENDPOINTS = Arrays.asList(
+			"/hw/login"
+	);
+	
+	private boolean exclude;
 	private final GatewayAudit auditor = new GatewayAudit();
 	
 	@Override
 	protected void beforeRequest(HttpServletRequest request, String message) {
-		auditor.setEndPoint(request.getRequestURI());
-		auditor.setRemoteAddress(request.getRemoteAddr());
-		auditor.setUsername(request.getRemoteUser());
-		log.info("Begin Gateway Audit of: {}", auditor);
-		auditor.setTime(System.currentTimeMillis());
+		String endpoint = request.getRequestURI();
+		exclude = EXCLUDED_ENDPOINTS.contains(endpoint);
+		if(!exclude) {
+			auditor.setEndPoint(endpoint);
+			auditor.setRemoteAddress(request.getRemoteAddr());
+			auditor.setUsername(request.getRemoteUser());
+			log.info("Begin Gateway Audit of: {}", auditor);
+			auditor.setTime(System.currentTimeMillis());
+		}
 	}
 
 	@Override
 	protected void afterRequest(HttpServletRequest request, String message) {
-		auditor.setDuration(System.currentTimeMillis() - auditor.getTime());
-		double time = (double)auditor.getDuration() / 1000;
-		log.info("Completed servicing request in: {} seconds", time);
+		if(!exclude) {
+			auditor.setDuration(System.currentTimeMillis() - auditor.getTime());
+			double time = (double)auditor.getDuration() / 1000;
+			log.info("Completed servicing request in: {} seconds", time);
+		}
 	}
 
 }
