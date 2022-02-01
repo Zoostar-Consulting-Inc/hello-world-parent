26f214ff6661924ffd1612f8695814f22c077831
zoostar <devops@zoostar.net>
2022-02-01 10:48:01 -0500

    HW-4: Abstract retrieve

--- src/main/java/net/zoostar/hw/web/api/AbstractApi.java
+++ src/main/java/net/zoostar/hw/web/api/AbstractApi.java
@@ -1,23 +1,50 @@
 package net.zoostar.hw.web.api;
 
+import javax.persistence.EntityNotFoundException;
+
+import net.zoostar.hw.service.EntityService;
+import net.zoostar.hw.service.SourceService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Persistable;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.http.ResponseEntity;
 
-import net.zoostar.hw.entity.EntityMapper;
-import net.zoostar.hw.service.EntityService;
+import lombok.Getter;
 
-public abstract class AbstractApi<E extends Persistable<T>, T> {
+public abstract class AbstractApi<R extends PagingAndSortingRepository<E, T>, E extends Persistable<T>, T> {
 
 	protected final Logger log = LoggerFactory.getLogger(this.getClass());
 
-	protected abstract EntityService<E, T> getEntityManager();
+	@Getter
+	@Autowired
+	private EntityService<R, E, T> entityManager;
 
-	protected ResponseEntity<E> create(EntityMapper<E> request) {
-		log.info("Creating entity from request: {}", request);
-		return new ResponseEntity<>(getEntityManager().
-				create(request.toEntity()), HttpStatus.CREATED);
+	@Getter
+	@Autowired
+	private SourceService<E, T> sourceManager;
+	
+	protected ResponseEntity<E> cudOperation(String sourceCode, String sourceId) {
+		sanitizeLogging(sourceCode, sourceId);
+		ResponseEntity<E> response = null;
+		try {
+			var entity = getEntityManager().retrieve(sourceCode, sourceId);
+			log.info("Entity retrieved: {}", entity);
+			response = sourceManager.update(entity);
+		} catch(EntityNotFoundException e) {
+			log.info("{}", e.getMessage());
+			response = getSourceManager().create(sourceCode, sourceId);
+		}
+		log.info("Response: {}", response);
+		return response;
 	}
+	
+	private void sanitizeLogging(String sourceCode, String sourceId) {
+		var param1 = sourceCode.replaceAll("[\n\r\t]", "_");
+		var param2 = sourceId.replaceAll("[\n\r\t]", "_");
+		log.info("Evaluating type of operation for source code {} and source id {}...", param1, param2);
+	}
+	
 }
--- src/main/java/net/zoostar/hw/web/api/ProductApi.java
+++ src/main/java/net/zoostar/hw/web/api/ProductApi.java
@@ -1,28 +1,22 @@
 package net.zoostar.hw.web.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import net.zoostar.hw.entity.Product;
+import net.zoostar.hw.repository.EntityRepository;
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.Getter;
-import net.zoostar.hw.entity.Product;
-import net.zoostar.hw.service.ProductService;
-import net.zoostar.hw.web.request.ProductRequest;
-
-@Getter
 @RestController
 @RequestMapping("/api/product")
-public class ProductApi extends AbstractApi<Product, String> {
+public class ProductApi extends AbstractApi<EntityRepository<Product, String>, Product, String> {
 
-	@Autowired
-	protected ProductService entityManager;
-
-	@PostMapping(path = "/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Product> create(@RequestBody ProductRequest request) {
-		return super.create(request);
+	@GetMapping(path = "/update/{sourceCode}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Product> cudOperation(@PathVariable String sourceCode, @RequestParam String sourceId) {
+		return super.cudOperation(sourceCode, sourceId);
 	}
 }
